78841406c0b2dc4915652d76ccaf6cc62015ef49
Brum <fbrump@gmail.com>
2018-01-02 23:36:01 -0200

    Created model Company

--- src/enterprise/company/models.py
+++ src/enterprise/company/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    """
+    Description: Company in the company
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    jobDescription = models.TextField(blank=True)
+
+    class Meta:
+        odering = ('name',)
