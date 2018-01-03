31dfbb4820ab2b366614f8a6685d7bba8bfc049b
Brum <fbrump@gmail.com>
2018-01-02 23:39:30 -0200

    Removed model Positon and keeping model Company with your properties

--- src/enterprise/company/models.py
+++ src/enterprise/company/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-class Position(models.Model):
-    """
-    Description: Position in the company
-    """
-    created = models.DateTimeField(auto_now_add=True)
-    code = models.CharField(max_length=20)
-    name = models.CharField(max_length=100)
-    jobDescription = models.TextField(blank=True)
+class Company(models.Model):
+	"""
+	Description: Company in the company
+	"""
+	created = models.DateTimeField(auto_now_add=True)
+	code = models.CharField(max_length=20)
+	name = models.CharField(max_length=100)
+	cnpj = models.CharField(max_length=15)
+	description = models.TextField(blank=True)
 
-    class Meta:
-        odering = ('name',)
+	class Meta:
+		odering = ('name',)
