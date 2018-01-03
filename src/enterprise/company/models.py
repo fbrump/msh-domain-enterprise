2c4c72e9208790938043dabfcc0292f9f7e2823c
Brum <fbrump@gmail.com>
2018-01-03 00:31:40 -0200

    Create migration for Company

--- src/enterprise/company/models.py
+++ src/enterprise/company/models.py
@@ -8,11 +8,10 @@ class Company(models.Model):
 	Description: Company
 	"""
 	created = models.DateTimeField(auto_now_add=True)
-	code = models.UUIDField¶(default=uuid.uuid4, editable=False)
+	code = models.UUIDField(default=uuid.uuid4, editable=False)
 	name = models.CharField(max_length=100)
 	cnpj = models.CharField(max_length=15)
 	description = models.TextField(blank=True)
 	url = models.URLField(max_length=500, blank=True)
-
 	class Meta:
-		odering = ('name',)
+		ordering = ('name',)
--- src/enterprise/company/serializers.py
+++ src/enterprise/company/serializers.py
@@ -4,17 +4,17 @@ from rest_framework import serializers
 from company.models import Company
 
 class CompanySerializer(serializers.Serializer):
-    code = serializers.UUIDField(format='hex', read_only=True)
-    name = serializers.CharField(min_length=3, max_length=100)
-    cnpj = serializers.CharField(max_length=15)
-    description = serializers.CharField()
-    url = serializers.URLField(max_length=500, allow_blank=True)
-    
-    def create(self, validated_data):
-    	"""
-    		Created new object and return validation.
-    	"""
-    	return Company.objects.created(***validated_data)
+	code = serializers.UUIDField(format='hex', read_only=True)
+	name = serializers.CharField(min_length=3, max_length=100)
+	cnpj = serializers.CharField(max_length=15)
+	description = serializers.CharField()
+	url = serializers.URLField(max_length=500, allow_blank=True)
+	
+	def create(self, validated_data):
+		"""
+			Created new object and return validation.
+		"""
+		return Company.objects.created(**validated_data)
 	def updated(self, instance, validated_data):
 		"""
 			Updated object instance and given validated data.
