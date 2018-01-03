665d5aa881dd8e1204b6839a0908e136affaf94d
Brum <fbrump@gmail.com>
2018-01-02 23:52:37 -0200

    Fixed models and serializer properties into documentation.

---
+++ src/enterprise/company/serializers.py
@@ -0,0 +1,11 @@
+# enterprise/company/serializers.py
+
+from rest_framework import serializers
+from company.models import Company
+
+class CompanySerializer(serializers.Serializer):
+    code = serializers.UUIDField()
+    name = serializers.CharField(min_length=3, max_length=100)
+    cnpj = serializers.CharField(max_length=15)
+    description = serializers.CharField()
+    
