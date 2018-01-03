fca5daeeca8f43b79781eeb74d6240150a34181a
Brum <fbrump@gmail.com>
2018-01-02 23:55:39 -0200

    Apply correct use of UUID

--- src/enterprise/company/serializers.py
+++ src/enterprise/company/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from company.models import Company
 
 class CompanySerializer(serializers.Serializer):
-    code = serializers.UUIDField()
+    code = serializers.UUIDField(default=uuid.uuid4, editable=False)
     name = serializers.CharField(min_length=3, max_length=100)
     cnpj = serializers.CharField(max_length=15)
     description = serializers.CharField()
