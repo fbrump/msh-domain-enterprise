5527898babd679799c52aa5774e2013223b539cc
Brum <fbrump@gmail.com>
2018-01-02 23:52:37 -0200

    Fixed models and serializer properties into documentation.

--- src/enterprise/company/models.py
+++ src/enterprise/company/models.py
@@ -1,3 +1,5 @@
+# enterprise/company/models.py
+
 from django.db import models
 
 class Company(models.Model):
@@ -5,10 +7,11 @@ class Company(models.Model):
 	Description: Company in the company
 	"""
 	created = models.DateTimeField(auto_now_add=True)
-	code = models.CharField(max_length=20)
+	code = models.UUIDField¶(max_length=20)
 	name = models.CharField(max_length=100)
 	cnpj = models.CharField(max_length=15)
 	description = models.TextField(blank=True)
+	url = models.URLField(max_length=500)
 
 	class Meta:
 		odering = ('name',)
