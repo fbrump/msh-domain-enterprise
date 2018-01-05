066ac6f48dd3dc19482aceb2260b8a4fd2816a6e
Brum <fbrump@gmail.com>
2018-01-04 23:15:24 -0200

    Added importations for working and build view

--- src/enterprise/company/views.py
+++ src/enterprise/company/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from company.models import Company
+from company.serializers import CompanySerializer
 
-# Create your views here.
+def company_list(request):
+	"""
+	"""
+
