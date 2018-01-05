31b04e61ce530e2d2945b49cbbe42b9421f3916d
Brum <fbrump@gmail.com>
2018-01-04 23:20:10 -0200

    Created first view with basic methods GET and POST.

--- src/enterprise/company/views.py
+++ src/enterprise/company/views.py
@@ -9,4 +9,14 @@ from company.serializers import CompanySerializer
 def company_list(request):
 	"""
 	"""
-
+	if request.method == 'GET':
+		companies = Company.objects.all()
+		serializer = CompanySerializer(companies, many=True)
+		return JsonResponse(serializer.data, safe=False)
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = CompanySerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=200)
+		return JsonResponse(serializer.errors, status=400)
