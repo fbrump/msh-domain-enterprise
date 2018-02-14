d99551cd0b64e69ff71e760a2b904496c14d1865
Brum <fbrump@gmail.com>
2018-02-14 01:03:02 -0200

    Created unite test for GET ALL and GET Single, apply implementation on views and URL

--- src/enterprise/position/urls.py
+++ src/enterprise/position/urls.py
@@ -1,11 +1,11 @@
 # enterprise/position/urls.py
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import *
+from . import views
 
 urlpatterns = [
-    #url(r'^positions/$', company_list),
-    #url(r'^positions/(?P<code>[0-9-a-z-A-Z]+)/$', company_detail),
+    url(r'^v1/positions/$', views.get_post_position, name='get_post_position'),
+    url(r'^v1/positions/(?P<code>[0-9-a-z-A-Z]+)/$', views.get_delete_update_postion, name='get_delete_update_postion'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
