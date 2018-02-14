8439fe660cfc8ddbc625aa2b027400051c8ae2f2
Brum <fbrump@gmail.com>
2018-02-14 00:05:55 -0200

    Created models tests with description and without description

--- src/enterprise/position/tests/test_models.py
+++ src/enterprise/position/tests/test_models.py
@@ -1,7 +1,7 @@
 # enterprise/position/tests/test_models.py
 
 from django.test import TestCase
-from ..models import Position
+from position.models import Position
 
 class PositionTest(TestCase):
 	""" Tests module for Position model """
@@ -16,8 +16,44 @@ class PositionTest(TestCase):
 		Position.objects.create(name='Frontend Developer', 
 								description= 'Apply usability with UI and UX using Angular and React')
 
-		def test_position_repr(self):
-			positionName = 'Web Developer'
-			webDeveloper = Position.objects.get(name=positionName)
-			self.assertEqual(
-				webDeveloper.__repr__(), webDeveloper.code + " - " + positionName)
+	def test_position_repr(self):
+		"""
+			Test method __repr__ for position.
+		"""
+		# arrange
+		positionName = 'Web Developer'
+		# act
+		webDeveloper = Position.objects.get(name=positionName)
+		# assert
+		self.assertEqual(
+			webDeveloper.__repr__(), str(webDeveloper.code) + " - " + positionName)
+	
+	def test_create_position_without_descritpion(self):
+		"""
+			Verify process to create an object without description
+		"""
+		# arrange
+		position = Position.objects.create(name='BI')
+		# act
+		position.save()
+		position_saved = Position.objects.get(name='BI')
+		#assert
+		self.assertIsNotNone(position_saved)
+		self.assertEqual(position.code,  position_saved.code)
+		self.assertIsNotNone(position.created)
+
+	def test_create_position_with_descritpion(self):
+		"""
+			Verify process to create an object with description
+		"""
+		# arrange
+		position = Position.objects.create(name='DBA', description='Data base administrator')
+		# act
+		position.save()
+		position_saved = Position.objects.get(name='DBA')
+		#assert
+		self.assertIsNotNone(position_saved)
+		self.assertEqual(position.code,  position_saved.code)
+		self.assertEqual('Data base administrator',  position_saved.description)
+		self.assertIsNotNone(position.created)
+
