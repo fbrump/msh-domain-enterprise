24a60e17653c899e2b506eb88e2695d9ae6e074e
Brum <fbrump@gmail.com>
2018-02-17 23:38:17 -0200

    Created unit test for mail manager.

--- src/enterprise/mail/tests/test_models.py
+++ src/enterprise/mail/tests/test_models.py
@@ -50,3 +50,51 @@ class MailEmployeerTest(TestCase):
 		mails = MailEmployeer.objects.filter(type_mail=type_mail)
 		# assert
 		self.assertTrue(len(mails) > 0)
+
+class MailManagerTest(TestCase):
+	"""
+		Suite case for tests in mail manager
+	"""
+	def setUp(self):
+		# Manager 1
+		MailManager.objects.create(
+			address='joaquim.perine@gmail.com',
+			type_mail=Mail.TYPE_PERSONAL)
+		MailManager.objects.create(
+			address='joaquim.perine@company.com',
+			type_mail=Mail.TYPE_PROFESSIONAL)
+		# Manager 2
+		MailManager.objects.create(
+			address='mery.banks@outlook.com',
+			type_mail=Mail.TYPE_PERSONAL)
+		MailManager.objects.create(
+			address='mery.banks@company.com',
+			type_mail=Mail.TYPE_PROFESSIONAL)
+		# Manager 3
+		MailManager.objects.create(
+			address='piedro.carmelo@company.com',
+			type_mail=Mail.TYPE_PROFESSIONAL)
+
+	def test_find_one_object(self):
+		# arrange
+		mail_address = 'mery.banks@company.com'
+		# act
+		mail_find = MailManager.objects.get(address=mail_address)
+		# assert
+		self.assertIsNotNone(mail_find)
+		self.assertEqual(mail_find.type_mail, Mail.TYPE_PROFESSIONAL)
+	def test_find_one_object_not_exists(self):
+		# arrange
+		mail_address = 'margarita.banks@company.com'
+		# act
+		mail_find = MailManager.objects.filter(address=mail_address)
+		# assert
+		self.assertIsNotNone(mail_find)
+		self.assertEqual(len(mail_find), 0)
+	def test_find_list_by_type(self):
+		# arrange
+		type_mail = Mail.TYPE_PERSONAL
+		# act
+		mails = MailManager.objects.filter(type_mail=type_mail)
+		# assert
+		self.assertTrue(len(mails) > 0)
