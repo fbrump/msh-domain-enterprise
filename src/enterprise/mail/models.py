329bf01107581f314cb7890f67c082873e789626
Brum <fbrump@gmail.com>
2018-02-17 22:57:10 -0200

    Created folder for tests (unittests) and model; deleted file tests.py

--- src/enterprise/mail/models.py
+++ src/enterprise/mail/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+# from dj.choices import Choices
+import uuid
 
-# Create your models here.
+class Mail(models.Model):
+	"""
+	Description: Model for mail object
+
+		Type of Mail - Personal, Professional
+	"""
+	TYPE_PERSONAL = 1
+	TYPE_PROFESSIONAL = 2
+	TYPES_CHOICES = (
+		(TYPE_PERSONAL, "Personal"),
+		(TYPE_PROFESSIONAL, "Progessional"),
+	)
+
+	created = models.DateTimeField(auto_now_add=True)
+	code = models.UUIDField(default=uuid.uuid4, editable=False)
+	name = models.CharField(max_length=100)
+	type_mail = models.IntegerField(
+		choices=TYPES_CHOICES,
+		default=TYPE_PERSONAL,
+	)
+	
+	class Meta:
+		abstract = True
