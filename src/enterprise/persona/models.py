1b4758aa3e608509a4ba09d9409483e64809a0ac
Brum <fbrump@gmail.com>
2018-05-09 23:55:58 -0300

    Created model persona and you relationship with Position

--- src/enterprise/persona/models.py
+++ src/enterprise/persona/models.py
@@ -1,15 +1,19 @@
 # enterprise/persona/models.py
 from django.db import models
+from position.models import Position
 
 import uuid
 
 class Persona(models.Model):
-	created = models.DateTimeField(auto_now_add=True)
-	code = models.UUIDField(default=uuid.uuid4, editable=False)
-	
-    class Meta:
-        verbose_name = "Persona"
-        verbose_name_plural = "Personas"
+	created = models.DateTimeField(auto_now_add=True, editable=False)
+	code = models.UUIDField(default=uuid.uuid4, editable=False, db_index=True)
+	name = models.CharField(max_length=100)
+	birth_date = models.DateTimeField()
+	position = models.ForeignKey(Position, on_delete=models.CASCADE)
 
-    def __str__(self):
-        abstract = True
+	class Meta:
+		verbose_name = "Persona"
+		verbose_name_plural = "Personas"
+
+	def __str__(self):
+		return self.name
